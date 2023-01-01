51e4dd3cf21f22327ab353ee67bd58de4a413ed1
Robi000 <88291984+Robi000@users.noreply.github.com>
2023-01-01 22:09:51 +0300

    Create Power - Mod Power.py

---
+++ Power - Mod Power.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+num = int(input())
+power = int(input())
+modules = int(input())
+print(num ** power)
+print((num ** power)%modules)
