ddb0b5cf22e5fbb45fbcf466f4a89114af48cd66
Robi000 <88291984+Robi000@users.noreply.github.com>
2023-01-04 20:04:06 +0300

    Time: 151 ms (5.03%), Space: 14 MB (38.58%) - LeetHub

---
+++ 0811-subdomain-visit-count/0811-subdomain-visit-count.py
@@ -0,0 +1,20 @@
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        dic = {}
+        for domain in cpdomains:
+            domain = domain.split()
+            pairs = domain[1].split(".")
+            print(domain , pairs)
+            upto = ""
+            for i in range(len(pairs)-1 , -1 , -1):
+                if i == len(pairs)-1:
+                    upto = pairs[i]
+                else:
+                    upto = pairs[i]+"."+upto
+                dic[upto] = dic.get(upto , 0) + int(domain[0])
+        ans = []
+        for key , val in dic.items():
+            ans.append(str(val) + " " + key)
+            
+        return(ans)
+        
