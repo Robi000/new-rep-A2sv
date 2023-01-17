63cada319c06462c1b34f7da0e0cee52cdd87184
Robi000 <88291984+Robi000@users.noreply.github.com>
2023-01-17 19:46:04 +0300

    Time: 1353 ms (5.86%), Space: 14.3 MB (84.60%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -5,14 +5,30 @@ class Solution:
         #         if heights[j] < heights[i]:
         #             heights[j] , heights[i] = heights[i] , heights[j]
         #             names[j] , names[i] = names[i] , names[j]
+        # for i in range(len(heights)):
+        #     mixx = i
+        #     for j in range( i +1 ,len(heights)):
+        #         if heights[j] > heights[mixx]:
+        #             mixx = j
+        #     if mixx != i:
+        #             heights[i] , heights[mixx] = heights[mixx] , heights[i]
+        #             names[i] , names[mixx] = names[mixx] , names[i]
+        # return names
         for i in range(len(heights)):
-            mixx = i
-            for j in range( i +1 ,len(heights)):
-                if heights[j] > heights[mixx]:
-                    mixx = j
-            if mixx != i:
-                    heights[i] , heights[mixx] = heights[mixx] , heights[i]
-                    names[i] , names[mixx] = names[mixx] , names[i]
+            j = i-1
+            while j >=0 and heights[j] < heights[i]:
+                if heights[j]<heights[i]:
+                    heights[i],heights[j] = heights[j], heights[i]
+                    names[i],names[j] = names[j], names[i]
+                    i -= 1
+                    j -= 1
+                else:
+                    break
+                    
+                
+            
+                                        
         return names
+        
 
                     
